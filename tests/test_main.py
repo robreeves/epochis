065224e6d34dfcb2950bddac065aafcd3e023c51
Rob Reeves <robreeves@users.noreply.github.com>
2021-05-21 14:19:32 -0700

    Fixed all pylint issues (#6)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+"""Tests for the epochis CLI
+"""
 import unittest
 import sys
 import io
@@ -17,23 +19,35 @@ def _capture_stdout(func):
 
 
 class TestMain(unittest.TestCase):
+    """Tests the CLI main method
+    """
     def test_months(self):
+        """Tests months since epoch
+        """
         out = _capture_stdout(lambda: main(["foo", "604m"]))
         self.assertEqual("2020-05", out)
 
     def test_days(self):
+        """Tests days since epoch
+        """
         out = _capture_stdout(lambda: main(["foo", "18412d"]))
         self.assertEqual("2020-05-30", out)
 
     def test_seconds(self):
+        """Tests seconds since epoch
+        """
         out = _capture_stdout(lambda: main(["foo", "1590792100s"]))
         self.assertEqual("2020-05-29 22:41:40", out)
 
     def test_milliseconds(self):
+        """Tests milliseconds since epoch
+        """
         out = _capture_stdout(lambda: main(["foo", "1590792100123ms"]))
         self.assertEqual("2020-05-29 22:41:40.123000", out)
 
     def test_trims_input(self):
+        """Tests that arguments with extra whitespace are handled
+        """
         out = _capture_stdout(lambda: main(["foo", "  1590792100123ms  "]))
         self.assertEqual("2020-05-29 22:41:40.123000", out)
 
